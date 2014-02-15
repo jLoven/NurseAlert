23b03690a28cde4bd1d3dff6ef922c7215d8d7bd
Jackie Loven <jloven@wpi.edu>
2014-02-15 17:26:21 -0500

    images updated and patient dialog box

--- src/com/jackieloven/thebasics/Patient.java
+++ src/com/jackieloven/thebasics/Patient.java
@@ -1,12 +1,30 @@
 package com.jackieloven.thebasics;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
 
 public class Patient extends Activity {
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.patient);
+		
+		ImageView imageBody = (ImageView) findViewById(R.id.imageView1);
+		
+		imageBody.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				startActivity(new Intent("com.jackieloven.thebasics.PATIENT_DIALOG"));
+			}
+		});
+		
+		
+		
 	}
 }
---
+++ src/com/jackieloven/thebasics/PatientDialog.java
@@ -0,0 +1,16 @@
+package com.jackieloven.thebasics;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PatientDialog extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.patient_dialog);
+		
+	}
+
+}
