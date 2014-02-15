d26354b487ff2aba4e0e5337cfca8cc4fb4a2ddc
Jackie Loven <jloven@wpi.edu>
2014-02-15 15:53:26 -0500

    display patient image

--- src/com/jackieloven/thebasics/Patient.java
+++ src/com/jackieloven/thebasics/Patient.java
@@ -1,10 +1,12 @@
 package com.jackieloven.thebasics;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class Patient extends Activity {
-
-	
-	
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.patient);
+	}
 }
