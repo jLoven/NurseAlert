31b3c50cc06ac2e0e84aad2d6f00ee76f5371586
Jackie Loven <jloven@wpi.edu>
2014-02-15 19:57:33 -0500

    immediately unloading splash screen

--- src/com/jackieloven/thebasics/Main.java
+++ src/com/jackieloven/thebasics/Main.java
@@ -1,24 +1,11 @@
 package com.jackieloven.thebasics;
 
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 
-
-
 public class Main extends Activity {
-
-	MediaPlayer logoMusic;
-
-	/**
-	 * logoMusic can now be used everywhere, not just where it's defined in
-	 * onCreate
-	 * 
-	 * @param TravisIsAwesome
-	 *            and don't redefine it down there now
-	 */
 
 	@Override
 	protected void onCreate(Bundle TravisIsAwesome) {
@@ -30,9 +17,6 @@ public class Main extends Activity {
 		 */
 		setContentView(R.layout.splash);
 
-		logoMusic = MediaPlayer.create(Main.this, R.raw.cant_hold_us);
-		logoMusic.start();
-
 		Thread logoTimer = new Thread() {
 			/** start a new thread and we're using the splash screen */
 			public void run() {
@@ -41,8 +25,7 @@ public class Main extends Activity {
 					 * want to try something. things are running at the same
 					 * time wanna only try something in case it doesn't work
 					 */
-					sleep(3500);
-					/** sleep for 3.5 seconds */
+
 					Intent menuIntent = new Intent(
 							"com.jackieloven.thebasics.MENU");
 					/**
@@ -56,13 +39,6 @@ public class Main extends Activity {
 					/**
 					 * use intent you just defined to open menu, matches
 					 * Manifest
-					 */
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-					/**
-					 * since "sleep" has an error, hover over it and insert
-					 * catch
 					 */
 				}
 
@@ -80,11 +56,7 @@ public class Main extends Activity {
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
-		logoMusic.release();
-		/**
-		 * LOL release: "kicks the sound down that hole in 300" just going to
-		 * stop the sound, too, if the sound is more than 5 seconds
-		 */
+
 	}
 
 	@Override
